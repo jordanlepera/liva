9ad8f4d213c12303998962852d36482d2f22b949
Jordan Le Pera <leperajordan@gmail.com>
2023-11-07 15:34:26 +0100

    ci(commitlint.config.js): ignore max line length for commit messages
    add a rule to ignore certain commit message containing a commit link, that cause an error when
    releasing because of body line max length exceeded

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,10 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  ignores: [
+    (message) =>
+      /\(\[[0-9a-fA-F]+]\(https:\/\/github\.com\/[^/]+\/[^/]+\/commit\/[0-9a-fA-F]+\)\)/g.test(
+        message,
+      ),
+    (message) => /^Bumps \[.+]\(.+\) from .+ to .+\.$/m.test(message),
+  ],
+};
