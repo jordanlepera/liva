e7d28f6f513a51f263a16d33e9d9859a12f67a3e
Jordan Le Pera <leperajordan@gmail.com>
2023-11-09 00:34:59 +0100

    feat(about.tsx): add a h1 tag

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,6 +3,7 @@ import { Main } from '@/templates/Main';
 
 const About = () => (
   <Main meta={<Meta title="Lorem ipsum" description="Lorem ipsum" />}>
+    <h1 className="text-2xl font-bold">Lorem ipsum</h1>
     <p>
       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
       recusandae quidem. Quaerat molestiae blanditiis doloremque possimus labore
