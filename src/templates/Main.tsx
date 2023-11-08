498a6c5438a537a789ec85d97472143f8ef3ae79
Jordan Le Pera <leperajordan@gmail.com>
2023-11-09 00:39:50 +0100

    feat(Main.tsx): new layout with new navigation
    new layout with responsive navigation mobile first and new links

--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -1,6 +1,11 @@
+import 'animate.css';
+
+import { Menu, X } from 'lucide-react';
+import Image from 'next/image';
 import Link from 'next/link';
 import type { ReactNode } from 'react';
 
+import logo from '@/public/assets/images/liva-logo-inline.svg';
 import { AppConfig } from '@/utils/AppConfig';
 
 type IMainProps = {
@@ -8,71 +13,151 @@ type IMainProps = {
   children: ReactNode;
 };
 
+type MenuItem = {
+  href: string;
+  text: string;
+  target?: string;
+};
+
+const menuItems: Array<MenuItem> = [
+  {
+    href: '/',
+    text: 'Accueil',
+  },
+  {
+    href: '/about',
+    text: 'Nos services',
+  },
+  {
+    href: '/galerie',
+    text: 'Galerie',
+  },
+  {
+    href: '/contact',
+    text: 'Contact',
+  },
+  {
+    href: 'https://www.facebook.com/profile.php?id=61551818345356',
+    text: 'Facebook',
+    target: '_blank',
+  },
+];
+
+let hideMenuTimeout: NodeJS.Timeout | null = null;
+
+function openMenu() {
+  document.getElementById('menu')?.classList.toggle('block');
+  document.getElementById('menu')?.classList.toggle('hidden');
+  document.getElementById('menu')?.classList.toggle('animate__fadeInLeftBig');
+  return () => {
+    if (hideMenuTimeout) {
+      clearTimeout(hideMenuTimeout);
+    }
+  };
+}
+
+function closeMenu() {
+  document.getElementById('menu')?.classList.toggle('animate__fadeInLeftBig');
+  document.getElementById('menu')?.classList.toggle('animate__fadeOutLeftBig');
+  hideMenuTimeout = setTimeout(() => {
+    document.getElementById('menu')?.classList.toggle('block');
+    document.getElementById('menu')?.classList.toggle('hidden');
+    document
+      .getElementById('menu')
+      ?.classList.toggle('animate__fadeOutLeftBig');
+  }, 100);
+  return () => {
+    if (hideMenuTimeout) {
+      clearTimeout(hideMenuTimeout);
+    }
+  };
+}
+
 const Main = (props: IMainProps) => (
-  <div className="w-full px-1 text-gray-700 antialiased">
+  <div className="w-full text-gray-700 antialiased">
     {props.meta}
 
-    <div className="mx-auto max-w-screen-md">
-      <header className="border-b border-gray-300">
-        <div className="pb-8 pt-16">
-          <h1 className="text-3xl font-bold text-gray-900">
-            {AppConfig.title}
-          </h1>
-          <h2 className="text-xl">{AppConfig.description}</h2>
-        </div>
-
-        <div className="flex justify-between">
-          <nav>
-            <ul className="flex flex-wrap text-xl">
-              <li className="mr-6">
-                <Link
-                  href="/"
-                  className="border-none text-gray-700 hover:text-gray-900"
-                >
-                  Home
-                </Link>
-              </li>
-              <li className="mr-6">
-                <Link
-                  href="/about/"
-                  className="border-none text-gray-700 hover:text-gray-900"
-                >
-                  About
-                </Link>
-              </li>
-              <li className="mr-6">
-                <Link
-                  href="/blog/"
-                  className="border-none text-gray-700 hover:text-gray-900"
-                >
-                  Blog
-                </Link>
-              </li>
-              <li className="mr-6">
-                <a
-                  className="border-none text-gray-700 hover:text-gray-900"
-                  href="https://github.com/ixartz/Next-js-Boilerplate"
-                >
-                  GitHub
-                </a>
-              </li>
+    <div className="mx-auto">
+      <header className="sticky top-0 z-10 border-b border-stone-300/30 bg-stone-100/30 backdrop-blur-xl">
+        <div>
+          <nav className="flex items-center justify-between py-4">
+            <div className="px-8">
+              <Link href="/">
+                <Image src={logo} alt="Liva Maçonnerie" priority />
+              </Link>
+            </div>
+            <div className="mx-8 flex flex-col items-center justify-center rounded-sm border-stone-500 transition-all focus:ring focus:ring-stone-300 xl:hidden">
+              <button
+                type="button"
+                className="flex flex-col items-center justify-center  border-none text-xs font-bold uppercase text-gray-700 hover:text-gray-900"
+                onClick={() => openMenu()}
+              >
+                <Menu />
+                Menu
+              </button>
+            </div>
+            <ul className="hidden flex-wrap text-xl xl:flex">
+              {menuItems.map((item, index) => {
+                return (
+                  <li
+                    className="mr-6"
+                    key={`menu-item-${index + Math.random()}`}
+                  >
+                    <Link
+                      href={item.href}
+                      className="border-none uppercase text-gray-700 hover:text-gray-900"
+                      target={item.target}
+                    >
+                      {item.text}
+                    </Link>
+                  </li>
+                );
+              })}
             </ul>
           </nav>
         </div>
       </header>
 
-      <main className="content py-5 text-xl">{props.children}</main>
+      <main className="text-xl">{props.children}</main>
 
       <footer className="border-t border-gray-300 py-8 text-center text-sm">
-        © Copyright {new Date().getFullYear()} {AppConfig.title}. Made with{' '}
-        <a href="https://creativedesignsguru.com">CreativeDesignsGuru</a>.
-        {/*
-         * PLEASE READ THIS SECTION
-         * I'm an indie maker with limited resources and funds, I'll really appreciate if you could have a link to my website.
-         * The link doesn't need to appear on every pages, one link on one page is enough.
-         * For example, in the `About` page. Thank you for your support, it'll mean a lot to me.
-         */}
+        © Copyright {new Date().getFullYear()} {AppConfig.title}
       </footer>
+      <nav
+        id="menu"
+        // eslint-disable-next-line tailwindcss/no-custom-classname
+        className="animate__animated animate__faster absolute left-0 top-0 z-10 h-full w-full bg-stone-300"
+      >
+        <button
+          className="absolute right-4 top-4 flex flex-col items-center justify-center"
+          onClick={() => closeMenu()}
+          type="button"
+        >
+          <X />
+          FERMER
+        </button>
+        <ul className="flex h-full flex-col items-center justify-center">
+          <div className="mb-8 max-w-[70%]">
+            <Link href="/" onClick={() => closeMenu()}>
+              <Image src={logo} alt="Liva Maçonnerie" priority />
+            </Link>
+          </div>
+          {menuItems.map((item, index) => {
+            return (
+              <li className="mb-8" key={`menu-item-${index + Math.random()}`}>
+                <Link
+                  href={item.href}
+                  className="border-none uppercase text-gray-700 hover:text-gray-900"
+                  target={item.target}
+                  onClick={() => closeMenu()}
+                >
+                  {item.text}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
     </div>
   </div>
 );
