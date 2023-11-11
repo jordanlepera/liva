cd98d45c542c08e687f8a9601c27e8e2077c2998
Jordan Le Pera <leperajordan@gmail.com>
2023-11-11 22:27:33 +0100

    feat(contact.tsx): create empty contact page

---
+++ src/pages/contact.tsx
@@ -0,0 +1,17 @@
+import { Meta } from '@/layouts/Meta';
+import { Main } from '@/templates/Main';
+
+const Contact = () => (
+  <Main
+    meta={<Meta title="Contactez nous" description="Formulaire de contact" />}
+  >
+    <div className="relative p-8">
+      <h1 className="w-full text-center text-xl font-bold md:text-4xl">
+        Contactez nous
+      </h1>
+      TO DO
+    </div>
+  </Main>
+);
+
+export default Contact;
