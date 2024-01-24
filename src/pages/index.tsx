73c66e75f18e31c1cfa27194f9efb5e162a1ec1f
Jordan Le Pera <leperajordan@gmail.com>
2024-01-24 02:23:49 +0100

    feat(Home page): add a new features block on the home page
    this new feature include a feature component that describe a feature inn a glance, and a features
    component that embed multiple features in a grid

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Contact from '@/components/contact/Contact';
+import { Features } from '@/components/features/Features';
 import { Hero } from '@/components/hero/Hero';
 import { Meta } from '@/layouts/Meta';
 import { Main } from '@/templates/Main';
@@ -13,14 +14,8 @@ const Index = () => (
     }
   >
     <Hero />
-    <main className="flex flex-col items-center justify-center bg-white p-8">
-      <section id="contact_index">
-        <h1 className="mb-8 text-center text-4xl font-bold text-gray-800">
-          Contactez nous !
-        </h1>
-        <Contact />
-      </section>
-    </main>
+    <Features />
+    <Contact />
   </Main>
 );
 
