4642a14dfee686acfab31253ecb344d13b90207c
Jordan Le Pera <leperajordan@gmail.com>
2024-04-10 17:20:02 +0200

    fix(config files): switch from commonjs configuration files to es modules files

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,12 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
+// /** @type {import('next-sitemap').IConfig} */
+// module.exports = {
+//   siteUrl: 'https://livamaconnerie.netlify.app/', // FIXME: Change to the production URL
+//   generateRobotsTxt: true,
+// };
+
+const nextSitemapConfig = {
   siteUrl: 'https://livamaconnerie.netlify.app/', // FIXME: Change to the production URL
   generateRobotsTxt: true,
 };
+
+export default nextSitemapConfig;
